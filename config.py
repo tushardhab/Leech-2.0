6fba7d0676efd9578ad775cb2b9f017ff604f6c6
Tushar Dhabale <128279576+tushardhab@users.noreply.github.com>
2025-04-25 23:16:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "24798261"
+api_hash = "fef280037f5759eccc540c6d7a279a14"
+bot_token = "7436355167:AAHPxzrJ5oKxVooSc8Ine8gvfVg1X4ndE_I"
+auth_users = [5347709348,6155478725]
+sudo_users = [5347709348]
